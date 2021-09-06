9b9afd031641b65e570338363aeb1ab78b8aff1d
Renan Santos <renan.stos077@gmail.com>
2021-09-06 17:08:08 -0300

    initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CreateRoomComponent } from './components/create-room/create-room.component';
+import { RoomDetailsComponent } from './components/room-details/room-details.component';
+import { RoomListComponent } from './components/room-list/room-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RoomDetailsComponent,
+    RoomListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/room.ts
@@ -0,0 +1,10 @@
+export class Room {
+    public Room() {}
+
+    id?: number;
+    name?: string;
+    date?:string
+    startHour?: string;
+    endHour?: string;
+    active?: boolean;
+}
