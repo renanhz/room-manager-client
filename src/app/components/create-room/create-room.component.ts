3f14c528ce34fa5cdf2fd0591167205d2ed674eb
Renan Santos <renan.stos077@gmail.com>
2021-09-06 17:25:41 -0300

    create room

--- src/app/components/create-room/create-room.component.ts
+++ src/app/components/create-room/create-room.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Room } from 'src/app/models/room';
+import { RoomService } from 'src/app/services/room.service';
 
 @Component({
   selector: 'app-create-room',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRoomComponent implements OnInit {
 
-  constructor() { }
+  room: Room = new Room();
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(private roomService: RoomService,
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  newRoom(): void {
+    this.submitted = false;
+    this.room = new Room();
+  }
+
+  save() {
+    this.roomService.createRoom(this.room)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.room = new Room();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();    
+  }
+
+  gotoList() {
+    this.router.navigate(['/rooms']);
   }
 
 }
